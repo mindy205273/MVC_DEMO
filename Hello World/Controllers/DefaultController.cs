3fbb1bed2333be57b6909caa0fec757a2392c388
Mindy <1105137113@nkust.edu.tw>
2019-04-18 14:45:35 +0800

    加入專案檔案。

---
+++ Hello World/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello_World.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Label = "Hello World";
+            return View();
+        }
+    }
+}
